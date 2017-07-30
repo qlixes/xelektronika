e90111cab009c90ede3c3e4f4c054bd08656ea49
qlixes <qlixess@gmail.com>
2017-07-31 01:51:06 +0700

    remove unused files

--- manage.py
+++
@@ -1,9 +0,0 @@
-from run import app
-from flask_apidoc.commands import GenerateApiDoc
-from flask_script import Manager
-
-manager = Manager(app)
-manager.add_command('apidoc', GenerateApiDoc())
-
-if __name__ == "__main__":
-manager.run()
